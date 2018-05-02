6279b943f5018830ef5faee31608cab56159d54c
Heorhi Sakovich <heorhi.sakovich@gmail.com>
2018-05-02 10:22:43 +0300

    in progress

--- init.sh
+++ init.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
+set -e
+
+JENKINS_DOWNLOAD_URL="http://mirrors.jenkins.io/war-stable/latest/jenkins.war"
+
 yum update -y
-yum install -y epel-release nginx
-yum install -y nginx
-systemctl start nginx
+yum install -y epel-release
+yum install -y nginx java-1.8.0-openjdk-headless
+
+id -u jenkins || useradd \
+    --home-dir /home/jenkins \
+    --create-home \
+    --shell /bin/bash \
+    jenkins
+cd /home/jenkins
+[ ! -f jenkins.war ] && su - jenkins  \
+    -c "curl -L $JENKINS_DOWNLOAD_URL -o jenkins.war"
+
+su - jenkins -c "java -jar jenkins.war"
